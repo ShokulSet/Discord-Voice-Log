d557e2df2570fe6d26d86da81b458f966e802312
Settapun Laoaree <Settapun.work@gmail.com>
2023-03-15 22:10:42 +0700

    Reset config

---
+++ reset.py
@@ -0,0 +1,12 @@
+import json
+
+config = {"guild_id": "channel_id"}
+exclude = {"guild_id": "list_of_channel_id"}
+
+with open("bot_config.json","wt") as w:
+        temp = json.dumps(config, indent=4)
+        w.write(temp)
+
+with open("exclusion.json","wt") as w:
+        temp = json.dumps(exclude, indent=4)
+        w.write(temp)
